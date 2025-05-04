db8a734e564a5837937f79f846a3b98559821904
Dora <doram275@icloud.com>
2025-05-04 14:38:46 +0200

    Add files via upload

--- Dora_main.py
+++ Dora_main.py
@@ -2,20 +2,26 @@ from Dora_EDA import MyModel
 
 model = MyModel()
 model.load_data(filepath="training_set_VU_DM.csv")
-high_corrs = model.get_highly_correlated_columns(threshold=0.7)
 
+#Finds columns that have a correlation value higher than 0.7
+high_corrs = model.get_highly_correlated_columns(threshold=0.7)
 for col1, col2, corr in high_corrs:
     print(f"High Correlation:{col1} vs {col2}: correlation = {corr:.3f}")
 
+#Remove one of highly correlated columns
+model.drop_columns(["booking_bool"])
+
+#Finds and removes columns that have more than 80% missing values
 high_missing_cols = model.get_columns_with_high_missing(threshold=0.8)
 model.drop_columns(high_missing_cols)
-model.drop_columns(["booking_bool"])
-print(len(f"Num high missing cols: {high_missing_cols})"))
 
-
+#Prints the new dataset after dropping columns
 print("New dataframe after dropping high-missing columns:")
 print(model.dataframe)
 
+#Prints out the summary of numericals columns such as range mean...
 model.summarize_numerical_columns()
 model.get_non_numerical_columns()
+
+#Prints out columns that have missing values still
 model.get_columns_with_missing_values()
